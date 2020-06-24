d7c64bbd27d511c960c91fdb802d142c368e7808
cardinalby <cardinalby@gmail.com>
2020-06-24 14:53:54 +0300

    checkIsUnchanged: diff added

--- src/bin/checkIsUnchanged.ts
+++ src/bin/checkIsUnchanged.ts
@@ -1,30 +1,42 @@
 #!/usr/bin/env node
 
 import {exec, ExecException} from 'child_process';
+import * as ghActions from '@actions/core';
 
-if (process.argv.length !== 3) {
-    process.stderr.write(process.argv.length < 3
-        ? 'File path is not provided'
-        : 'Too many arguments'
-    );
-    process.exit(1);
+function checkIsUnchanged() {
+    if (process.argv.length !== 3) {
+        ghActions.setFailed(process.argv.length < 3
+            ? 'File path is not provided'
+            : 'Too many arguments');
+        return;
+    }
+
+    const filePath = process.argv[2];
+    if (filePath.length === 0 || filePath.indexOf('"') !== -1) {
+        ghActions.setFailed('Invalid file path provided');
+        return;
+    }
+
+    exec(
+        `git status --porcelain "${filePath}"`,
+        (error: ExecException | null, stdout: string, stderr: string) => {
+            if (error || stdout || stderr) {
+                ghActions.setFailed(
+                    `${filePath} is changed during the workflow. ` +
+                    "Please ensure, you didn't forget to build and pack your action before commit. Diff:"
+                );
+                if (error) {
+                    return;
+                }
+                exec(
+                    `git diff "${filePath}"`,
+                    (error: ExecException | null, stdout: string) => {
+                        process.stdout.write(stdout);
+                    })
+            } else {
+                ghActions.info(`No changes found in ${filePath}`);
+            }
+        });
 }
 
-const filePath = process.argv[2];
-if (filePath.length === 0 || filePath.indexOf('"') !== -1) {
-    process.stderr.write('Invalid file path provided');
-    process.exit(1);
-}
-
-exec(
-    `git status --porcelain "${filePath}"`,
-    (error: ExecException | null, stdout: string, stderr: string) => {
-        if (error || stdout || stderr) {
-            process.stderr.write(
-                `${filePath} is changed during the workflow. ` +
-                "Please ensure, you didn't forget to build and pack your action before commit"
-            );
-            process.exit(1);
-        }
-        process.stdout.write(`No changes found in ${filePath}`);
-    });
+checkIsUnchanged();
